f7fc8092a33cea9eda44a08676bd5da6063ff6c3
xupeng <564347621@qq.com>
2019-03-13 11:42:55 +0800

    协议户明细

---
+++ src/main/java/cn/hbis/erp/modular/system/controller/ProtocolAccountDetailsController.java
@@ -0,0 +1,11 @@
+package cn.hbis.erp.modular.system.controller;
+
+import cn.stylefeng.roses.core.base.controller.BaseController;
+
+/**
+ * 协议户明细控制器
+ *
+ *
+ */
+public class ProtocolAccountDetailsController extends BaseController {
+}
---
+++ src/main/java/cn/hbis/erp/modular/system/service/ProtocolAccountDetailsService.java
@@ -0,0 +1,19 @@
+package cn.hbis.erp.modular.system.service;
+
+import cn.hbis.erp.modular.system.entity.ProtocolAccountDetails;
+import cn.hbis.erp.modular.system.mapper.ProtocolAccountDetailsMapper;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+
+import javax.annotation.Resource;
+
+/**
+ * <p>
+ * 协议户明细表 服务实现类
+ * </p>
+ *
+ *
+ *
+ */
+public class ProtocolAccountDetailsService extends ServiceImpl<ProtocolAccountDetailsMapper, ProtocolAccountDetails> {
+
+}
