8ec589e1a932fb482abf48c569c694a907677a6c
xupeng <564347621@qq.com>
2019-03-14 14:37:59 +0800

    查询

--- src/main/java/cn/hbis/erp/modular/system/mapper/ProtocolAccountDetailsMapper.java
+++ src/main/java/cn/hbis/erp/modular/system/mapper/ProtocolAccountDetailsMapper.java
@@ -2,6 +2,11 @@ package cn.hbis.erp.modular.system.mapper;
 
 import cn.hbis.erp.modular.system.entity.ProtocolAccountDetails;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+import java.util.Map;
 
 /**
  * <p>
@@ -12,4 +17,10 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
  *
  */
 public interface ProtocolAccountDetailsMapper extends BaseMapper<ProtocolAccountDetails> {
+    /**
+     * 根据条件查询协议户明细列表
+     */
+    Page<Map<String, Object>> searchProtocolAccountDetailList(@Param("page") Page page, @Param("mainSalesRegional") String mainSalesRegional,
+                                         @Param("beginTime") String beginTime, @Param("endTime") String endTime,
+                                         @Param("protocolYear") String protocolYear, @Param("steelMills") String steelMills);
 }
