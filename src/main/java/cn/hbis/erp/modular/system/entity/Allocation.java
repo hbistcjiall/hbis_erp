c4b25482458c65560581d973aa8f1f28dd16deb3
zhangb11 <981478524@qq.com>
2019-03-22 14:11:22 +0800

    合同进度修改

--- src/main/java/cn/hbis/erp/modular/system/entity/Allocation.java
+++ src/main/java/cn/hbis/erp/modular/system/entity/Allocation.java
@@ -8,8 +8,48 @@ package cn.hbis.erp.modular.system.entity;
 public class Allocation {
     private String pzName;
     private Integer yield;
-    private Integer planName;
+    private Integer planNum;
     private Double schedule;
     private String cxName;
+
+    public String getPzName() {
+        return pzName;
+    }
+
+    public void setPzName(String pzName) {
+        this.pzName = pzName;
+    }
+
+    public Integer getYield() {
+        return yield;
+    }
+
+    public void setYield(Integer yield) {
+        this.yield = yield;
+    }
+
+    public Integer getPlanNum() {
+        return planNum;
+    }
+
+    public void setPlanNum(Integer planNum) {
+        this.planNum = planNum;
+    }
+
+    public Double getSchedule() {
+        return schedule;
+    }
+
+    public void setSchedule(Double schedule) {
+        this.schedule = schedule;
+    }
+
+    public String getCxName() {
+        return cxName;
+    }
+
+    public void setCxName(String cxName) {
+        this.cxName = cxName;
+    }
 }
 
