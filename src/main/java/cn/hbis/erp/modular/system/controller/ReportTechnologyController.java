7212e63fbe2966e2d7c0bfc03684b237e2ae4a81
xupeng <564347621@qq.com>
2019-03-23 19:04:41 +0800

    导出一部分

--- src/main/java/cn/hbis/erp/modular/system/controller/ReportTechnologyController.java
+++ src/main/java/cn/hbis/erp/modular/system/controller/ReportTechnologyController.java
@@ -1,22 +1,27 @@
 package cn.hbis.erp.modular.system.controller;
 
 import cn.hbis.erp.core.util.DateUtil;
+import cn.hbis.erp.core.util.ExportExcel;
 import cn.hbis.erp.modular.system.entity.ReportVarietySteelBean;
 import cn.hbis.erp.modular.system.service.ReportTechnologyService;
+import cn.stylefeng.roses.core.util.ToolUtil;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
 /**
  * 子公司品种钢情况
@@ -27,6 +32,7 @@ import java.util.Map;
 @RestController
 @RequestMapping("/reportTechnology")
 public class ReportTechnologyController {
+    private static String EXPORT_XLSX_FILE_SUFFIX = ".xlsx";
 
     @Autowired
     private ReportTechnologyService reportTechnologyService;
@@ -71,17 +77,46 @@ public class ReportTechnologyController {
             @ApiImplicitParam(name = "queryDate" ,value = "月份",dataType ="String" )
     })
     @PostMapping(value = "exportSubsidiaryVarietySteel")
-    public void exportVarietySteelState(String queryDate, HttpServletRequest request, HttpServletResponse response){
-        if(null == queryDate ||"".equals(queryDate)){
-            queryDate = DateUtil.getNowMonth();
-            String startDate = DateUtil.getFirstDayOfMonth(queryDate);
-            String endDate = DateUtil.getLastDayOfMonth(queryDate);
-        }else{
-            String startDate = DateUtil.getFirstDayOfMonth(queryDate);
-            String endDate = DateUtil.getLastDayOfMonth(queryDate);
+    public void export(Map<String, Object> map ,String queryDate, HttpServletResponse response) {
+        String startDate = DateUtil.getFirstDayOfMonth(queryDate);
+        String endDate = DateUtil.getLastDayOfMonth(queryDate);
+        List<ReportVarietySteelBean> resultList = reportTechnologyService.subsidiaryVarietySteel(startDate, endDate);
+
+        List<Map<String, Object>> list = new ArrayList<>();
+        for(int i = 0; i < resultList.size(); i++) { //数据库为空,遍历了100000个
+            Map<String, Object> temp_ = new HashMap<>();
+            temp_.put("companyName", resultList.get(i).getCompanyName());
+            temp_.put("totalSteel", resultList.get(i).getTotalSteel());
+            temp_.put("totalSteelVarieties", resultList.get(i).getTotalSteelVarieties());
+
+            temp_.put("featuresProducts", resultList.get(i).getFeaturesProducts());
+            temp_.put("highProducts", resultList.get(i).getHighProducts());
+            temp_.put("steelVarieties", resultList.get(i).getSteelVarieties());
+            list.add(temp_);
         }
-        //开始导出
-        //reportTechnologyService.exportSubsidiaryVarietySteel(startDate, endDate, request, response);
+        ExportExcel<List<Map<String, Object>>> exportExcel = new ExportExcel<>();
+        DateFormat format = new SimpleDateFormat("yyyyMMddHHmmss");
+        StringBuffer filename = null;
+        if(ToolUtil.isEmpty(map.get("fileName"))) {
+            filename = new StringBuffer();
+            filename.append("子公司品种钢情况");
+            filename.append(format.format(new Date()));
+        } else {
+            filename = ((StringBuffer)map.get("fileName")).insert(0,format.format(new Date()).substring(0,7));
+        }
+        if(ToolUtil.isEmpty(map.get("excel_type"))) {
+            filename.append(EXPORT_XLSX_FILE_SUFFIX);
+        } else {
+            filename.append(map.get("excel_type"));
+        }
+
+        try {
+            FileOutputStream out = new FileOutputStream("D:/"+filename.toString());
+            exportExcel.exportXSExcelByColumn(filename.toString(), new String[] {"单位", "钢材总量(吨)", "品种钢总量(吨)","特色战略产品(吨)", "高端产品(吨)", "一般品种钢(吨)"},
+                    new String[] {"companyName", "totalSteel", "totalSteelVarieties","featuresProducts", "highProducts", "steelVarieties"},
+                    list, out ,null);
+        } catch (IOException e) {
+        }
     }
     /**
      * @Title subsidiaryVarietySteelItem
