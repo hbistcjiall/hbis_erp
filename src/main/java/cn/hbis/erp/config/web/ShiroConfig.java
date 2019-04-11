3d788ed0642abfc0a857a86d4f23fa4c9b993466
xupeng <564347621@qq.com>
2019-04-11 11:54:15 +0800

    修改bug

--- src/main/java/cn/hbis/erp/config/web/ShiroConfig.java
+++ src/main/java/cn/hbis/erp/config/web/ShiroConfig.java
@@ -124,7 +124,7 @@ public class ShiroConfig {
         /**
          * 默认的登陆访问url
          */
-        shiroFilter.setLoginUrl("/index.html");
+        shiroFilter.setLoginUrl("/docs.html");
         /**
          * 登陆成功后跳转的url
          */
