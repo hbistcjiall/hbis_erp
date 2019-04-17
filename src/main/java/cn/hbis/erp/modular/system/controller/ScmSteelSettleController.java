e9358969a20efa07dab3d376e2950f20802e0966
yaojiaqi12138 <47956022+yaojiaqi12138@users.noreply.github.com>
2019-04-17 10:25:16 +0800

    app
    app

--- src/main/java/cn/hbis/erp/modular/system/controller/ScmSteelSettleController.java
+++ src/main/java/cn/hbis/erp/modular/system/controller/ScmSteelSettleController.java
@@ -10,11 +10,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.List;
+import java.util.*;
 
 /**
  * <p>
@@ -29,6 +35,10 @@ import java.util.List;
 public class ScmSteelSettleController {
     @Autowired
     private ScmSteelSettleService scmSteelSettleService;
+    public static final String GET_URL = "http://price.oltest-hbistc.com:8080/priceweb/priceSellPrice/oneSpacesPhone.htm";
+    public static final String GET_URL1 = "http://price.oltest-hbistc.com:8080/priceweb/dologin.htm?account=10100050&password=123456";
+    public static final String POST_URL = "http://price.oltest-hbistc.com:8080/priceweb/priceSellPrice/oneSpacesPhone.htm";
+
 
     @ApiOperation(value = "月度产线报表")
     @ApiImplicitParams({
@@ -254,5 +264,72 @@ public class ScmSteelSettleController {
     public List<ScmSteelSettle> getCxNamePzg(String companyId){
         return  scmSteelSettleService.getCxNamePzg(companyId);
     }
+
+
+
+    @ApiOperation(value = "测试")
+    @PostMapping("httpURLConnectionPOST")
+    @Async
+
+    public Map httpURLConnectionPOST() throws IOException {
+
+
+                URL url = new URL(POST_URL);
+
+                // 将url 以 open方法返回的urlConnection  连接强转为HttpURLConnection连接  (标识一个url所引用的远程对象连接)
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();// 此时cnnection只是为一个连接对象,待连接中
+
+                // 设置连接输出流为true,默认false (post 请求是以流的方式隐式的传递参数)
+                connection.setDoOutput(true);
+
+                // 设置连接输入流为true
+                connection.setDoInput(true);
+
+                // 设置请求方式为post
+                connection.setRequestMethod("POST");
+
+                // post请求缓存设为false
+                connection.setUseCaches(false);
+
+                // 设置该HttpURLConnection实例是否自动执行重定向
+                connection.setInstanceFollowRedirects(true);
+
+                // 设置请求头里面的各个属性 (以下为设置内容的类型,设置为经过urlEncoded编码过的from参数)
+                // application/x-javascript text/xml->xml数据 application/x-javascript->json对象 application/x-www-form-urlencoded->表单数据
+                connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+
+                // 建立连接 (请求未开始,直到connection.getInputStream()方法调用时才发起,以上各个参数设置需在此方法之前进行)
+                connection.connect();
+
+                // 创建输入输出流,用于往连接里面输出携带的参数,(输出内容为?后面的内容)
+                DataOutputStream dataout = new DataOutputStream(connection.getOutputStream());
+                String parm = "fkdat=" + URLEncoder.encode("2019-02", "utf-8")+"&"+"companyId="+URLEncoder.encode("9196", "utf-8"); //URLEncoder.encode()方法  为字符串进行编码
+                // 将参数输出到连接
+                dataout.writeBytes(parm);
+
+                // 输出完成后刷新并关闭流
+                dataout.flush();
+                dataout.close(); // 重要且易忽略步骤 (关闭流,切记!)
+
+                System.out.println(connection.getResponseCode());
+
+                // 连接发起请求,处理服务器响应  (从连接获取到输入流并包装为bufferedReader)
+                BufferedReader bf = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                String line;
+                StringBuilder sb = new StringBuilder(); // 用来存储响应数据
+                Map map = new HashMap();
+                // 循环读取流,若不到结尾处
+                while ((line = bf.readLine()) != null) {
+                    sb.append(line);
+                    System.out.println(sb);
+
+                }
+                bf.close();    // 重要且易忽略步骤 (关闭流,切记!)
+                connection.disconnect(); // 销毁连接
+                map.put(sb,"sb");
+                return map;
+    }
+
+
 }
 
