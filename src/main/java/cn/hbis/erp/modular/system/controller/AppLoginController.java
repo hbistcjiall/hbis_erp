b024309607dd49a1e959290caa755517490a6490
zhangb11 <981478524@qq.com>
2019-05-22 15:39:45 +0800

    微服务登录

--- src/main/java/cn/hbis/erp/modular/system/controller/AppLoginController.java
+++ src/main/java/cn/hbis/erp/modular/system/controller/AppLoginController.java
@@ -29,14 +29,13 @@ public class AppLoginController {
     @ApiOperation(value = "app登录")
     @PostMapping("appLogin")
     @ApiImplicitParams({
-            @ApiImplicitParam(name = "POST_URL", value = "端口地址", dataType = "String"),
-            @ApiImplicitParam(name = "userName", value = "登录名", dataType = "String"),
+            @ApiImplicitParam(name = "account", value = "登录名", dataType = "String"),
             @ApiImplicitParam(name = "password", value = "密码", dataType = "String")
     })
     @Async
-    public Map<String, Object> appLogin(String POST_URL, String userName, String password) throws IOException {
+    public Map<String, Object> appLogin(String account, String password) throws IOException {
 
-        URL url = new URL(POST_URL);
+        URL url = new URL("http://18.0.128.1:8080/priceweb/loginPort/doLogin.htm");
 
         // 将url 以 open方法返回的urlConnection  连接强转为HttpURLConnection连接  (标识一个url所引用的远程对象连接)
         HttpURLConnection connection = (HttpURLConnection) url.openConnection();// 此时cnnection只是为一个连接对象,待连接中
@@ -68,7 +67,7 @@ public class AppLoginController {
 
         String parm = "";
         //拼接查询条件字符串  //URLEncoder.encode()方法  为字符串进行编码
-        parm = "userName=" + URLEncoder.encode(userName,"UTF-8") + "&" + "password=" + password;
+        parm = "account=" + URLEncoder.encode(account,"UTF-8") + "&" + "password=" + password;
         // 将参数输出到连接
         dataout.writeBytes(parm);
 
